cc98d980bc8600c8627524ae4a802c08e6986f4a
jay0000000000000000 <johay03@naver.com>
2023-10-08 09:59:41 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int a, b;
+  
+  printf("input two integars :");
+  scanf("%i %i", &a, &b);
+  
+  printf("%i + %i = %i \n", a,b, a+b);
+  printf("%i - %i = %i \n", a,b, a-b);
+  printf("%i * %i = %i \n", a,b, a*b);
+  printf("%i / %i = %i \n", a,b, a/b);
+  printf("%i %% %i = %i \n", a,b, a%b);
+  
+  
+  system("PAUSE");	
+  return 0;
+}
