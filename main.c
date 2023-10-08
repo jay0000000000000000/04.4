e171f4717eb0269cc6dc8485b1d2fee83d0a7cff
jay0000000000000000 <johay03@naver.com>
2023-10-08 11:18:48 +0900

    06

--- main.c
+++ main.c
@@ -5,21 +5,17 @@
 
 int main(int argc, char *argv[])
 {
-  unsigned int x;
-  int b;
+  int input;
+  int sec, min, hour;
   
-  printf("input a number : ");
-  scanf("%ui", &x);
+  printf("input second:");
+  scanf("%d", &input);
   
-  for (b=0; x != 0; x >>=1) // -> x = x >> 1
-  {
-      if (x & 1)
-      {
-         b++;
-      }
-  }
+  sec = input%60;
+  min = (input%3600) / 60;
+  hour = input/3600;
   
-  printf("The result is : %i\n", b);
+  printf("The time is %d : %d : %d\n", hour, min, sec);
   
   system("PAUSE");	
   return 0;
